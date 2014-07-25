a103cb2f78977258bd5fb800a0f642413560d264
David Jinkins <david.jinkins@gmail.com>
2014-07-25 11:53:50 +0200

    I added parameter definitions to the top

--- params.m
+++ params.m
@@ -1,5 +1,20 @@
 % This file holds parameter values for use in running serach and learning model code
 
+% Link between X parameter values and parameters used in the model
+    % lnF         =  scale_h+log(X(1));
+    % delta       =  X(2);
+    % scale_h     =  X(3);
+    % scale_f     =  scale_h + log(X(4));
+    % beta        =  X(5);
+    % a           =  X(6);
+    % b           =  X(7);
+    % L_z         =  X(8);
+    % D_z         =  X(9);
+    % L_b         =  X(10);
+    % gam         =  X(11)*(1+beta)/beta;
+    % cost scalar =  X(12);
+    % sig p       =  X(13);
+    
 % for scripts that take a single parameter vector
 X = [...
    0.1943930477380   0.2453104276277   9.0422436478983 0.2662888213532   0.0416788862843   0.8430293904631 4.4510131294695   0.4127794741502   0.0985318562579 1.2768491022205   0.3800889628219   432.0297211614394 0.3925683743937;...
