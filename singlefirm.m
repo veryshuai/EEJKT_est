5c6d741e1aaef62918b0961e5ea5a14e3e326900
David Jinkins <david.jinkins@gmail.com>
2014-08-03 11:00:17 +0200

    added comments to calculation of macro shocks in singlefirm.m

--- singlefirm.m
+++ singlefirm.m
@@ -25,19 +25,19 @@ sh_val_f = zeros(mms,3);
 if breakflag == 0
 
     %preallocate
-    ind(:,:) = repmat([0,0,0,0,-1,-1,-1,-1,zeros(1,2),-1,-1],mms,1);
-    deathmat(:) = zeros(mms,1);
+    ind(:,:) = repmat([0,0,0,0,-1,-1,-1,-1,zeros(1,2),-1,-1],mms,1); % holds state
+    deathmat(:) = zeros(mms,1); % holds dummy for a firm death
     
     %put in "actual" X_f's (foreign macro shocks)
     for k = 1:size(actual_f,1)-2
-        ind(1+k,1) = burn+k;
-        ind(1+k,4)  = actual_f(k+2,2);
+        ind(k+1,1) = burn+k; % time of the shock
+        ind(k+1,4)  = actual_f(k+2,2); % value of the shock
     end
 
     %put in "actual" X_h's (home macro shocks)
     for k = 1:size(actual_h,1)-2
-        ind(1+k,1) = burn+k;
-        ind(1+k,3)  = actual_h(k+2,2);
+        ind(k+1,1) = burn+k; %time of the shock 
+        ind(k+1,3)  = actual_h(k+2,2); % value of the shock
     end 
 
     %get Phi's (self productivities)
@@ -66,26 +66,26 @@ if breakflag == 0
     end
 
 %put in burn period macro shocks (backwards!)
-% NEED TO FIX THIS SO THAT ALL FIRMS FACE SAME SHOCKS!
 %foreign
-k = 1;
-[obin,max_mat_violation,vio] = step(obin,mms,max_mat_violation); if vio == 1 breakflag = 1; end
-next = actual_f(3,2);
-exp_inv_temp = log(agg_shocks(k))/-L_f;
-k = k+1;
+
+k = 1; % placeholder for shock values in preallocated vector (so all firms get the same macro shock)
+[obin,max_mat_violation,vio] = step(obin,mms,max_mat_violation); if vio == 1 breakflag = 1; end 
+next = actual_f(3,2); %needs to end in the first actual shock
+exp_inv_temp = log(agg_shocks(k))/-L_f; %time since last change
+k = k+1; 
 gap = exp_inv_temp;
-    while gap<burn+1
-        ind(obin,1) = burn+1-gap;
-        ind(obin,4) = next+1-2*(agg_shocks(k)<.5*(1+(next-x_size-1)/x_size));
-        k = k+1;
-        next = ind(obin,4);
-        [obin,max_mat_violation,vio] = step(obin,mms,max_mat_violation); if vio == 1 breakflag = 1; break; end
-        exp_inv_temp = log(agg_shocks(k))/-L_f;
-        k = k+1;
-        gap = gap + exp_inv_temp;
-    end
-ind(obin,1) = 2e-12; %put in correct starting value
-ind(obin,4) = next; 
+while gap<burn+1 %check to make sure there is still time for another shock
+    ind(obin,1) = burn+1-gap; %time of shock
+    ind(obin,4) = next+1-2*(agg_shocks(k)<.5*(1+(next-x_size-1)/x_size)); %value of shock
+    k = k+1;
+    next = ind(obin,4);
+    [obin,max_mat_violation,vio] = step(obin,mms,max_mat_violation); if vio == 1 breakflag = 1; break; end
+    exp_inv_temp = log(agg_shocks(k))/-L_f; %time until next shock
+    k = k+1;
+    gap = gap + exp_inv_temp;
+end
+ind(obin,1) = 2e-12; %put in correct starting time
+ind(obin,4) = next; %put in correct starting value 
 [obin,max_mat_violation,vio] = step(obin,mms,max_mat_violation); if vio == 1 breakflag = 1; end
 
 %home
@@ -94,16 +94,16 @@ g = 1;
 exp_inv_temp = log(agg_shocks(k))/-L_h;
 k = k+1;
 gap = exp_inv_temp;
-    while gap<burn+1
-        ind(obin,1) = burn+1-gap;
-        ind(obin,3) = next+1-2*(agg_shocks(k)<.5*(1+(next-x_size-1)/x_size));
-        k = k+1;
-        next = ind(obin,3);
-        [obin,max_mat_violation,vio] = step(obin,mms,max_mat_violation); if vio == 1 breakflag = 1; break; end
-        exp_inv_temp = log(agg_shocks(k))/-L_h;
-        k = k+1;
-        gap = gap + exp_inv_temp;
-    end
+while gap<burn+1
+    ind(obin,1) = burn+1-gap;
+    ind(obin,3) = next+1-2*(agg_shocks(k)<.5*(1+(next-x_size-1)/x_size));
+    k = k+1;
+    next = ind(obin,3);
+    [obin,max_mat_violation,vio] = step(obin,mms,max_mat_violation); if vio == 1 breakflag = 1; break; end
+    exp_inv_temp = log(agg_shocks(k))/-L_h;
+    k = k+1;
+    gap = gap + exp_inv_temp;
+end
 ind(obin,1) = 1e-12; %put in correct starting value
 ind(obin,3) = next; 
 [obin,max_mat_violation,vio] = step(obin,mms,max_mat_violation); if vio == 1 breakflag = 1; end
@@ -179,7 +179,7 @@ obin_fix = zeros(S,1);
                         cum_spell = spell + cum_spell;
                     end
                 end
-            lag = deathind(n)+1;    
+            lag = deathind(n)+1;
             end
         end
 
