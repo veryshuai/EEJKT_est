d7e80531689526928e56e0b67a2afca7295ae460
David Jinkins <david.jinkins@gmail.com>
2014-08-08 09:28:34 +0200

    added value calculation files

--- call_val.m
+++ call_val.m
@@ -5,5 +5,8 @@
     for k=1:20
         simulate(X, 'results/val_results', 6);
         val_mat{k} = calc_val(beta); %get the value of the network
+        save('results/network_val_calc_results')
     end
-    save('results/network_val_calc_results')
+
+    % Summarize
+     
