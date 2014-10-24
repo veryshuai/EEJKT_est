c968e5489ed101c490091b0f0a0a41576575ef0d
David Jinkins <david.jinkins@gmail.com>
2014-10-24 10:19:03 -0400

    making counterfactual files

---
+++ cfs_loop.m
@@ -0,0 +1,30 @@
+function dat = cfs_loop(task,savename)
+%This function loops simulations of a particular counterfactual, and returns the generated data
+
+    %Simulation number
+    sim_no = 5;
+    
+    dat = cell(sim_no,2);
+    
+    for k=1:sim_no
+        switch task 
+            case 1
+              simulate(X, 'results/sim_results', 1);
+              load('results/cf_sim_results');              
+              dat{k,1} = sale_f_mat; dat{k,2} = cli_no; 
+            case 2
+              cost_dec_trans(1);
+            case 3
+              simulate(X, 'results/mac_bump_results', 3)
+              load('results/cf_sim_results');              
+              dat{k,1} = sale_f_mat; dat{k,2} = cli_no; 
+            case 4
+              red_var_trans(1);
+            case 5
+              search_dec_trans(1);
+        end
+    end
+
+    save(savename);
+
+end
